e5e767a3cb589524a1db28e68326a63e706cb4a3
Hashem Ahmed <hhashemaahmed@gmail.com>
2025-02-10 20:26:36 +0200

    update

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
       <div className="App">
           <Header/>
         <Routes>
-          <Route path="/belegal" element={<Home />} />
+          <Route path="/" element={<Home />} />
           <Route path="/services" element={<Services />} />
         </Routes>
       </div>
